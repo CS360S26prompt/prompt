4197c0bd4c99e7d79de5090fd0a17d5c7991bd58
Momin-Fahed <27100082@lums.edu.pk>
2026-02-16 14:37:34 +0500

    Circle class

---
+++ app/src/main/java/com/example/prompt/Circle.java
@@ -0,0 +1,24 @@
+package com.example.prompt;
+
+public class Circle extends Shape {
+
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle at (" + x + "," + y + ") with radius " + radius);
+    }
+}
